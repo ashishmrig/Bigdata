4d8f6fca000e0ef103c89cb4d0561f28460e546a
Ashish Mrig <ashmrig@gmail.com>
2022-03-23 09:37:00 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,13 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+df = pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+    })
 
-df = np.random.randn(10, 20)
-st.dataframe(df)
+option = st.selectbox(
+    'Which number do you like best?',
+     df['first column'])
+
+'You selected: ', option
