a92abd2f950c4e80f10d51e9e28d51fe37c9ffff
Ashish Mrig <ashmrig@gmail.com>
2022-03-24 13:30:09 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ import plotly.express as px
 
 
 # Add a selectbox to the sidebar:
-st.title('NE Med Faculty Summary Report FY2021' )
+st.title('NE-Med Faculty Summary Report FY2021' )
 
 df = pd.read_csv('./Sample_NeMedFile.csv')
 group_labels = df["Clinical Productivity"]
