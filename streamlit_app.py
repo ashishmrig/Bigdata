fad54e120a7a2c2f4a1999e7ae6513c645067f17
Ashish Mrig <ashmrig@gmail.com>
2022-03-24 11:12:52 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import plotly.figure_factory as ff
+
 
 # Add a selectbox to the sidebar:
 st.title('NE Med Faculty Summary Report FY2021' )
