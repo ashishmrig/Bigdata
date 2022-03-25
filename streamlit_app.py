322f44a6aeef0a0594d38f02956c54aab884a8e2
Ashish Mrig <ashmrig@gmail.com>
2022-03-25 09:56:32 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,36 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib as mp
+import matplotlib.pyplot as plt
 
 
 # Add a selectbox to the sidebar:
 st.title('NE-Med Faculty Summary Report FY2021' )
 
 df = pd.read_csv('./Sample_NeMedFile.csv')
+new_df = df.T.reset_index()
+new_df.columns = new_df.iloc[0]
+new_df = new_df.drop(df.index[0])
+
+new_df["RVUs"] = new_df["RVUs"].astype(int)
+new_df["Clinic Visits"] = new_df["Clinic Visits"].astype(int)
+new_df['# of Clinics'] = new_df['# of Clinics'].astype(int)
+new_df['# Hours at VA'] = new_df['# Hours at VA'].astype(int)
+new_df['wRVU / Clinic'] = new_df['wRVU / Clinic'].astype(int)
+new_df['wRVU / Visit'] = new_df['wRVU / Visit'].astype(int)
+new_df['# visits / clinic'] = new_df['# visits / clinic'].astype(int)
+new_df['Beginning Balance'] = new_df['Beginning Balance'].astype(int)
+new_df['Ending Balance'] = new_df['Ending Balance'].astype(int)
+
+plt.figure( figsize = (15,9) )
+plt.plot(new_df["Clinical Productivity"], new_df["RVUs"])
+plt.xlabel('Clinical Productivity', fontsize=15)
+plt.ylabel('RVUs', fontsize=15)
+st.write(plt.show())
+
 group_labels = df["Clinical Productivity"]
 
-st.line_chart(df)
+
 
 
 
